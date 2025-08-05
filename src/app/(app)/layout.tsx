820862dcb1fb7a25097f660975b5e2ccf05a0863
Ibnu Wibowo <ibnuw83@gmail.com>
2025-08-05 13:08:20 +0000

    masih belum berubah

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -198,8 +198,10 @@ export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10 pt-28">
-        {children}
+      <main className="flex flex-1 flex-col bg-muted/40 pt-16">
+        <div className="flex-1 space-y-4 p-4 md:space-y-8 md:p-8">
+          {children}
+        </div>
       </main>
     </div>
   );

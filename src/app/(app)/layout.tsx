26a8942c067abc651f4cfd51737fbc6640157dd2
Ibnu Wibowo <ibnuw83@gmail.com>
2025-08-05 13:05:44 +0000

    tulisan dasbor admin ketutup header, tolong di bawahkan

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -198,7 +198,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10 pt-20">
+      <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10 pt-24">
         {children}
       </main>
     </div>
